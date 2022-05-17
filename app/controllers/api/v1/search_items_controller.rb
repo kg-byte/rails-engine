e52aef6b53fea7b1843e44175ca3daa3b84d301d
Kim G <xiaole.guo@yahoo.com>
2022-05-17 14:58:13 -0600

    refactor search items and search merchants controller

--- app/controllers/api/v1/search_items_controller.rb
+++ app/controllers/api/v1/search_items_controller.rb
@@ -16,12 +16,8 @@ class Api::V1::SearchItemsController < ApplicationController
     return render json: {data: {error: 'Cannot send both name and max_price'}}, status: 400 if item_params[:name] && item_params[:max_price]
     return render json: {data: {error: 'Cannot send both name and max_price and min_price'}}, status: 400 if item_params[:name] && item_params[:min_price] && item_params[:max_price]
     return render json: {data: {error: 'Cannot send both name and min_price'}}, status: 400 if item_params[:name] && item_params[:min_price]
-
-    if params[:max_price] && params[:min_price] 
-      return render json: {data: {error: 'max_price cannot be less than min_price'}}, status: 400 if item_params[:max_price].to_i < item_params[:min_price].to_i
-    end
+    return render json: {data: {error: 'max_price cannot be less than min_price'}}, status: 400 if params[:max_price] && params[:min_price] && item_params[:max_price].to_i < item_params[:min_price].to_i
     
-
     if params[:name] && params[:name] != ''
       item = Item.where("lower(name) like ?", "%#{params[:name].downcase}%").order(:name).first
     elsif params[:min_price] && params[:min_price] != '' && !params[:max_price]
@@ -32,11 +28,8 @@ class Api::V1::SearchItemsController < ApplicationController
       item = Item.where(unit_price: params[:min_price].to_i..params[:max_price].to_i).order(:name).first
     end
 
-    if item 
-      return render json: Api::V1::ItemSerializer.new(item), status: :ok
-    else 
-      return render json: {data: {error: 'Item not found'}}
-    end
+    return render json: Api::V1::ItemSerializer.new(item), status: :ok if item
+    return render json: {data: {error: 'Item not found'}} if !item
   end
 
   private
