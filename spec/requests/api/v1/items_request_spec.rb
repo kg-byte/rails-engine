c8a8390af43af04c533a2b9fef3b8ab32c2058e1
Kim G <xiaole.guo@yahoo.com>
2022-05-18 14:25:57 -0600

    complete pagination feature

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,18 +2,53 @@ require 'rails_helper'
 
 describe "Items API" do 
 
-  it 'sends all items' do 
+  it 'sends 20 items from page 1 by default' do 
     merchant1 = create(:merchant)
-    create_list(:item,3, merchant_id: merchant1.id)
+    create_list(:item,20, merchant_id: merchant1.id)
     merchant2 = create(:merchant)
-    create_list(:item,3, merchant_id: merchant2.id)
+    create_list(:item,20, merchant_id: merchant2.id)
 
     get "/api/v1/items"
 
     expect(response).to be_successful
     items = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(items.count).to eq(6)
+    expect(items.count).to eq(20)
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq('item')
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
+
+  it 'sends 10 items from page 3 by default' do 
+    merchant1 = create(:merchant)
+    create_list(:item,20, merchant_id: merchant1.id)
+    merchant2 = create(:merchant)
+    create_list(:item,20, merchant_id: merchant2.id)
+
+    get "/api/v1/items?page=3&per_page=10"
+
+    expect(response).to be_successful
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(items.count).to eq(10)
     items.each do |item|
       expect(item).to have_key(:id)
       expect(item[:id]).to be_a(String)
