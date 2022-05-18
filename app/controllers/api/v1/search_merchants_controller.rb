449df10b77ca66f59645ebb3fdd1589ccef3ef33
Kim G <xiaole.guo@yahoo.com>
2022-05-18 12:37:37 -0600

    refactor merchant model and controller

--- app/controllers/api/v1/search_merchants_controller.rb
+++ app/controllers/api/v1/search_merchants_controller.rb
@@ -1,20 +1,48 @@
 class Api::V1::SearchMerchantsController < ApplicationController
   def index
-    if params[:name] && params[:name] != ''
-      merchants = Merchant.where("lower(name) like ?", "%#{params[:name].downcase}%")
-  	  render json: Api::V1::MerchantSerializer.new(merchants)
+    if edge_cases_conditions
+      edge_case_resposne
+    elsif name_search
+  	  render json: Api::V1::MerchantSerializer.new(Merchant.search_all_by_name(params[:name])) 
     end
-    render json: {data: {error: 'Parameter cannot be missing'}}, status: 400 if !params[:name]
-    render json: {data: {error: 'Parameter cannot be empty'}}, status: 400 if params[:name] ==''
   end
 
   def show
-    if params[:name] && params[:name] != ''
-      merchant = Merchant.where("lower(name) like ?", "%#{params[:name].downcase}%").order(:name).first
+    if edge_cases_conditions
+      edge_case_resposne
+    elsif name_search
+      merchant = Merchant.search_one_by_name(params[:name])
       render json: Api::V1::MerchantSerializer.new(merchant), status: :ok if merchant
       render json: {data: {error: 'Merchant not found'}} if !merchant
-    end
-      render json: {data: {error: 'Parameter cannot be missing'}}, status: 400 if !params[:name]
-      render json: {data: {error: 'Parameter cannot be empty'}}, status: 400 if params[:name] ==''
+    end 
   end
+
+  private 
+    def name_search
+      params[:name] && params[:name] != ''
+    end
+
+    def no_params
+      !params[:name]
+    end
+
+    def empty_params
+       params[:name] ==''
+    end
+
+    def edge_cases_conditions
+      no_params || empty_params
+    end
+
+    def edge_case_resposne
+      return render json: Api::V1::ErrorSerializer.format_error(error_messages[:missing_param]), status: 400 if no_params 
+      return render json: Api::V1::ErrorSerializer.format_error(error_messages[:empty_param]), status: 400 if empty_params 
+    end
+
+    def error_messages
+      messages = Hash.new 
+      messages[:missing_param]='Parameter cannot be missing'
+      messages[:empty_param] = 'Parameter cannot be empty'
+      messages
+    end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.search_all_by_name(name)
+    Merchant.where("lower(name) like ?", "%#{name.downcase}%")
+  end
+
+  def self.search_one_by_name(name)
+    search_all_by_name(name).order(:name).first
+  end
 end
