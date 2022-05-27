6a98019054989e01ad8330e5d063eb6d276528b7
Kim G <xiaole.guo@yahoo.com>
2022-05-26 21:35:47 -0600

    wip in the middle of avenue by date range. semi working but numbers do not match

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,4 +7,5 @@ class Invoice < ApplicationRecord
   enum status: {"cancelled" => 0, "in progress" => 1, "completed" => 2}
 
 
+
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,9 +5,8 @@ class InvoiceItem < ApplicationRecord
   enum status: {"packaged" => 0, "pending" => 1, "shipped" => 2}
 
   def self.total_revenue_during(start_date, end_date)
-    start_d = Time.zone.parse('2012-03-27')
-    end_d = Time.zone.parse('2012-03-29')
-    invoice_items = InvoiceItem.where(created_at: start_d..end_d).sum('quantity*unit_price')
+    start_d = Time.zone.parse('2012-03-09')
+    end_d = Time.zone.parse('2012-03-24')
+    InvoiceItem.joins(:invoice).where(invoices: {created_at: start_d..end_d}).sum('quantity*unit_price')
   end
-
 end
